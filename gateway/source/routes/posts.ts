b35328e329a4a4a0dbc3020ca7e8c3f44d79c8f4
Raphael Picard <raphaelpicard@outlook.fr>
2022-05-10 15:22:51 +0200

    delete gateway

--- gateway/source/routes/posts.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../controllers/posts';
-const router = express.Router();
-
-router.get('/posts', controller.getPosts);
-router.post('/addPosts', controller.addPost);
-
-export = router;
